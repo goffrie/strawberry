b1c6ee3bcab6f7fe22107597742f3b6e90b99f65
Geoffry Song <goffrie@gmail.com>
2020-02-16 00:12:11 -0800

    client: good code

--- client/src/gameAPI.tsx
+++ client/src/gameAPI.tsx
@@ -15,35 +15,25 @@ export type MakeRoomReply = {
     room: string,
 };
 
-export async function callList(room: string, version: number, signal?: AbortSignal): Promise<ListReply> {
-    const resp = await fetch("/list", {
+async function call(endpoint: string, data: object, signal?: AbortSignal): Promise<any> {
+    const resp = await fetch(endpoint, {
         method: "POST",
         cache: "no-cache",
         headers: {"content-type": "application/json"},
-        body: JSON.stringify({ room, version }),
+        body: JSON.stringify(data),
         signal,
     });
     return await resp.json();
 }
 
-export async function callCommit(room: string, version: number, data: RoomState, signal?: AbortSignal): Promise<CommitReply> {
-    const resp = await fetch("/commit", {
-        method: "POST",
-        cache: "no-cache",
-        headers: {"content-type": "application/json"},
-        body: JSON.stringify({ room, version, data }),
-        signal,
-    });
-    return await resp.json();
+export function callList(room: string, version: number, signal?: AbortSignal): Promise<ListReply> {
+    return call("/list", { room, version }, signal);
 }
 
-export async function callMakeRoom(data: RoomState, signal?: AbortSignal): Promise<MakeRoomReply> {
-    const resp = await fetch("/make_room", {
-        method: "POST",
-        cache: "no-cache",
-        headers: {"content-type": "application/json"},
-        body: JSON.stringify({ data }),
-        signal,
-    });
-    return await resp.json();
+export function callCommit(room: string, version: number, data: RoomState, signal?: AbortSignal): Promise<CommitReply> {
+    return call("/commit", { room, version, data }, signal);
+}
+
+export function callMakeRoom(data: RoomState, signal?: AbortSignal): Promise<MakeRoomReply> {
+    return call("/make_room", { data }, signal);
 }
