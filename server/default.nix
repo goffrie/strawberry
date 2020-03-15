adde655642fe213b2ee84ada4e6945bf2edd99c1
Geoffry Song <goffrie@gmail.com>
2020-03-15 00:18:11 -0700

    server: default.nix: don't test

--- server/default.nix
+++ server/default.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.rustPlatform.buildRustPackage {
-    pname = "globby";
-    version = "0.1.0";
-    src = ./.;
-    cargoSha256 = "0pj7bww4bl12bmsf3025i1va5sbn6wvl5hzx8qpr5irkd1h7dnrh";
-    cargoSha256Version = 2;
+  pname = "globby";
+  version = "0.1.0";
+  src = ./.;
+  cargoSha256 = "0pj7bww4bl12bmsf3025i1va5sbn6wvl5hzx8qpr5irkd1h7dnrh";
+  cargoSha256Version = 2;
+  checkPhase = "true";
 }
