e8c1af6294e74a9fe3226b3df430af77394dc174
Geoffry Song <goffrie@gmail.com>
2020-03-15 02:30:39 -0700

    server: dump/restore server state

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,18 @@
 use parking_lot::RwLock;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::ToSocketAddrs;
+use std::path::Path;
 use std::sync::Arc;
 use std::time::Duration;
-use std::{env, io};
+use std::{env, fs, io};
+use tokio::signal::ctrl_c;
+use tokio::signal::unix::{signal, SignalKind};
 use tokio::time::timeout;
 use warp::http::StatusCode;
 use warp::reply::Reply;
@@ -18,6 +22,7 @@ mod words;
 
 const SIZE_LIMIT: u64 = 1024 * 1024; // 1MB
 const LIST_TIMEOUT: Duration = Duration::from_secs(45);
+const DUMP_FILENAME: &str = "room_data.json";
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -33,6 +38,15 @@ async fn main() -> io::Result<()> {
             "Second argument should be static directory",
         )
     })?;
+    let data_path = env::args().nth(3).ok_or_else(|| {
+        io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Third argument should be data directory",
+        )
+    })?;
+    let data_path = Path::new(&data_path);
+    let dump_path = data_path.join(DUMP_FILENAME);
+    std::fs::create_dir_all(&data_path)?;
     let listen_addr = listen_addr
         .to_socket_addrs()?
         .into_iter()
@@ -43,8 +57,14 @@ async fn main() -> io::Result<()> {
                 "Listen address resolved to nothing",
             )
         })?;
-    println!("Listening on {}", listen_addr);
-    let state = Arc::new(State::default());
+    let initial_state = match fs::File::open(&dump_path) {
+        Ok(f) => serde_json::from_reader(io::BufReader::new(f))?,
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => Inner::default(),
+        Err(e) => return Err(e),
+    };
+    let state = Arc::new(State {
+        inner: RwLock::new(initial_state),
+    });
     let state_ = Arc::clone(&state);
     let list = warp::path!("list")
         .and(warp::body::json())
@@ -68,31 +88,53 @@ async fn main() -> io::Result<()> {
                 success: state_.commit(req),
             })
         });
+    let state_ = Arc::clone(&state);
     let make_room = warp::path!("make_room")
         .and(warp::body::json())
-        .map(move |req| warp::reply::json(&state.make_room(req)));
+        .map(move |req| warp::reply::json(&state_.make_room(req)));
     let stateful_routes = warp::post()
         .and(warp::body::content_length_limit(SIZE_LIMIT))
         .and(list.or(commit).or(make_room));
     let static_routes = warp::get().and(warp::fs::dir(static_dir));
-    warp::serve(stateful_routes.or(static_routes))
-        .run(listen_addr)
-        .await;
+    let mut sigterm = signal(SignalKind::terminate())?;
+    let ctrl_c = ctrl_c();
+    let (listen_addr, server) =
+        warp::serve(stateful_routes.or(static_routes)).bind_ephemeral(listen_addr);
+    println!("Listening on {}", listen_addr);
+    tokio::select! {
+        _ = server => panic!("server cannot exit"),
+        _ = sigterm.recv() => {
+            println!("Received SIGTERM, shutting down");
+        },
+        _ = ctrl_c => {
+            println!("Received Ctrl-C, shutting down");
+        },
+    };
+    println!("Dumping server state to {}", data_path.display());
+    let inner = state.inner.read();
+    let random_path = data_path.join(format!("tmp.{:032x}", rand::thread_rng().gen::<u128>()));
+    let mut dump_file = io::BufWriter::new(fs::File::create(&random_path)?);
+    serde_json::to_writer(&mut dump_file, &*inner)?;
+    let dump_file = dump_file.into_inner()?;
+    dump_file.sync_all()?;
+    fs::rename(&random_path, &dump_path)?;
     Ok(())
 }
 
-#[derive(Default)]
 struct State {
     inner: RwLock<Inner>,
 }
 
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 struct Inner {
     rooms: HashMap<String, Room>,
 }
 
+#[derive(Serialize, Deserialize)]
 struct Room {
     version: u32,
+    #[serde(skip)]
+    #[serde(default = "new_event")]
     event: Arc<futures_intrusive::channel::OneshotBroadcastChannel<Infallible>>,
     data: Box<serde_json::value::RawValue>,
 }
@@ -163,7 +205,7 @@ impl State {
         room.data = req.data;
         room.version += 1;
         room.event.close();
-        room.event = Arc::new(futures_intrusive::channel::OneshotBroadcastChannel::new());
+        room.event = new_event();
         true
     }
 
@@ -181,7 +223,7 @@ impl State {
                 Entry::Vacant(v) => {
                     v.insert(Room {
                         version: 1,
-                        event: Arc::new(futures_intrusive::channel::OneshotBroadcastChannel::new()),
+                        event: new_event(),
                         data: req.data,
                     });
                 }
@@ -189,4 +231,8 @@ impl State {
             return MakeRoomReply { room };
         }
     }
+}
+
+fn new_event() -> Arc<futures_intrusive::channel::OneshotBroadcastChannel<Infallible>> {
+    Arc::new(futures_intrusive::channel::OneshotBroadcastChannel::new())
 }
