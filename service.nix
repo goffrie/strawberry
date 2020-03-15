7ec4e29af88b65fa4b851169556c037c1a58ad98
Geoffry Song <goffrie@gmail.com>
2020-03-15 06:04:08 +0000

    fix service.nix

--- service.nix
+++ service.nix
@@ -2,44 +2,45 @@
 with lib;
 let
   cfg = config.services.strawberry;
-  client = import ./client { pkgs };
-  server = import ./server { pkgs };
+  client = import ./client { inherit pkgs; };
+  server = import ./server { inherit pkgs; };
 in {
   options.services.strawberry = {
-    enabled = mkOption {
+    enable = mkOption {
       type = types.bool;
       default = false;
     };
     listen = mkOption {
-      type = types.string;
+      type = types.str;
       example = "127.0.0.1:8080";
       description = "The address on which to listen";
     };
   };
-  config = mkIf cfg.enabled {
+  config = mkIf cfg.enable {
     systemd.services.strawberry = {
-    description = "Strawberry";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    environment.RUST_BACKTRACE = "1";
-    serviceConfig = {
-      ExecStart = "${server}/bin/globby ${cfg.listen} ${client}";
-      StandardOutput = "syslog";
-      StandardError = "syslog";
-      SyslogIdentifier = "strawberry";
-      DynamicUser = true;
-      PermissionsStartOnly = true;
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      PrivateDevices = true;
-      PrivateUsers = true;
-      PrivateTmp = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectControlGroups = true;
-      RestrictAddressFamilies = "AF_INET AF_INET6";
-      Restart = "always";
-      RestartSec = "5s";
+      description = "Strawberry";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      environment.RUST_BACKTRACE = "1";
+      serviceConfig = {
+        ExecStart = "${server}/bin/globby ${cfg.listen} ${client}";
+        StandardOutput = "syslog";
+        StandardError = "syslog";
+        SyslogIdentifier = "strawberry";
+        DynamicUser = true;
+        PermissionsStartOnly = true;
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateDevices = true;
+        PrivateUsers = true;
+        PrivateTmp = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6";
+        Restart = "always";
+        RestartSec = "5s";
+      };
     };
   };
 }
