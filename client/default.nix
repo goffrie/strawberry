2c486655a07b1678e48f31a099921683dcd42952
Geoffry Song <goffrie@gmail.com>
2022-01-28 01:43:58 -0800

    client: fix nix build

--- client/default.nix
+++ client/default.nix
@@ -1,13 +1,6 @@
-{ pkgs ? import <nixpkgs> {}
-, yarn2nix ? import (pkgs.fetchFromGitHub {
-    owner = "moretea";
-    repo = "yarn2nix";
-    rev = "9e7279edde2a4e0f5ec04c53f5cd64440a27a1ae";
-    sha256 = "0zz2lrwn3y3rb8gzaiwxgz02dvy3s552zc70zvfqc0zh5dhydgn7";
-  }) { inherit pkgs; },
-}:
+{ pkgs ? import <nixpkgs> {} }:
 
-yarn2nix.mkYarnPackage {
+pkgs.mkYarnPackage {
   name = "client";
   src = ./.;
   packageJSON = ./package.json;
@@ -15,7 +8,7 @@ yarn2nix.mkYarnPackage {
   nativeBuildInputs = [ pkgs.jq ];
   buildPhase = ''
     mkdir deps/client/node_modules_2
-    ln -s $(realpath deps/client/node_modules)/{*,.bin} deps/client/node_modules_2/
+    ln -s $(realpath node_modules)/{*,.bin} deps/client/node_modules_2/
     rm deps/client/node_modules
     mv deps/client/node_modules_2 deps/client/node_modules
 
