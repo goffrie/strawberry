f46e5566e46e2b48507fb7c821fe6823f02bfb40
Geoffry Song <goffrie@gmail.com>
2022-12-05 22:01:42 +0000

    server: update dependencies

--- server/default.nix
+++ server/default.nix
@@ -4,10 +4,10 @@ let
 rustPkgs = pkgs // import (pkgs.fetchFromGitHub {
   owner = "oxalica";
   repo = "rust-overlay";
-  rev = "26d7210f4801d9dc8f48b5d8bc34df1cf41284c8";
-  sha256 = "0pggczamd1ilbs9aid5w6nxlww6cbvv6ibaxj7x3vk4185xbd16s";
+  rev = "18823e511bc85ed27bfabe33cccecb389f9aa92d";
+  sha256 = "uuKbbv0L+QoXiqO7METP9BihY0F7hJqGdKn7xDVfyFw=";
 }) rustPkgs pkgs;
-rustc = rustPkgs.rust-bin.stable."1.52.1".default.override { targets = ["x86_64-unknown-linux-musl"]; };
+rustc = rustPkgs.rust-bin.stable."1.65.0".default.override { targets = ["x86_64-unknown-linux-musl"]; };
 in
 (pkgs.rustPlatform.buildRustPackage.override { inherit rustc; }) {
   pname = "globby";
@@ -17,7 +17,7 @@ in
     { name = "Cargo.lock"; path = "${./Cargo.lock}"; }
     { name = "src"; path = "${./src}"; }
   ];
-  cargoSha256 = "1456rgp8d2gfscbphzi7j5slzdjrw8ysm9yy8blzi5p6dbxln1p3";
+  cargoSha256 = "hbYB/WiIvyViEITlvSLvlCdgBZxb8Q5Sw23wqtOQIqE=";
   cargoSha256Version = 2;
   checkPhase = "true";
   target = "x86_64-unknown-linux-musl";
