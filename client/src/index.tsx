7b70940e8eb2bc3fced6df494b09b60e21ae28de
Geoffry Song <goffrie@gmail.com>
2024-02-20 01:11:38 +0000

    client: switch ReactDOM.render to createRoot

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const initialRoom = window.location.hash ? window.location.hash.substr(1) : '';
 
-ReactDOM.render(<App initialRoom={initialRoom} />, document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App initialRoom={initialRoom} />);
 
